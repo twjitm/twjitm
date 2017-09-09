70606e4133d650bc461485d96a3b20ee54940736
唐文江 <1029718215@qq.com>
2017-09-09 12:27:23 +0800

    Create ReceiptController.java

---
+++ src/main/java/com/twjitm/receipt/controller/ReceiptController.java
@@ -0,0 +1,48 @@
+package com.twjitm.receipt.controller;
+
+import com.twjitm.receipt.entity.Equzlize;
+import com.twjitm.receipt.entity.Receipt;
+import com.twjitm.receipt.enums.ReceiptStateType;
+import com.twjitm.receipt.service.IReceiptService;
+import com.twjitm.user.entity.User;
+import com.twjitm.user.service.IUserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/9/9.
+ */
+@Controller
+@RequestMapping("receipt")
+public class ReceiptController {
+@Resource
+public IReceiptService receiptService;
+@Resource
+public IUserService userService;
+
+@RequestMapping("init")
+ public String init(HttpServletRequest request){
+    List<Receipt>list= receiptService.getReceiptByState(ReceiptStateType.DOING);
+    request.setAttribute("list",list);
+    return "/receipt/init";
+ }
+ @RequestMapping("equzlize")
+ @ResponseBody
+ public  List<Equzlize> equzlize(HttpServletRequest request){
+     List<Receipt>list= receiptService.getReceiptByState(ReceiptStateType.DOING);
+    List<User> users= userService.getUser();
+    List<Long> uIds=new ArrayList<Long>();
+    for(int i=0;i<users.size();i++){
+        uIds.add(users.get(i).getId());
+    }
+    List<Equzlize> equzlizes= receiptService.getEquzlizeList(list,uIds);
+     request.setAttribute("equzlizes",equzlizes);
+     return equzlizes;
+ }
+}
